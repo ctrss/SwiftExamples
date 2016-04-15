3ea7eaa0d48191dab9fbdb93e421fbaf1a7b21b7
ctrss <cthrss@dreamwiz.com>
2016-04-15 13:51:09 +0900

    Create Single View Application

---
+++ SwiftiPhoneSimpleApplication/SwiftiPhoneSimpleApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftiPhoneSimpleApplication
+//
+//  Created by kakao on 2016. 4. 15..
+//  Copyright © 2016년 staryun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
